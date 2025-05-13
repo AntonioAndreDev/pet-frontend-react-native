2e531f7598720b7b50aa500225ebdaf2f59125e1
Antonio Andre <antonioandre1008@gmail.com>
2025-05-13 15:19:55 -0500

    :sparkles: feat: inclui header

--- app/(app)/home.jsx
+++ app/(app)/home.jsx
@@ -25,12 +25,30 @@ export default function Home() {
     );
 
   return (
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={{ paddingHorizontal: 16 }}>
-        <Text>Home</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <View
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          zIndex: 10,
+          height: 82,
+          backgroundColor: '#facc15',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          paddingTop: 40,
+          paddingHorizontal: 16,
+        }}>
+        <Text className="text-slate-700" style={{ fontSize: 18, fontWeight: 'bold' }}>
+          Pet
+        </Text>
+      </View>
+
+      <ScrollView contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 52 }}>
         <View>
           {posts.map((post) => (
-            <View key={post.id_post} style={{ marginBottom: 30 }}>
+            <View key={post.id_post} style={{ marginBottom: 20 }}>
               <Post post={post} />
             </View>
           ))}
