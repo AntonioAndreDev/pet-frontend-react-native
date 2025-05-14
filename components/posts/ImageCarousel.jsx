40883d88e82e209e6cd8f34a7ffb8729831b04d1
Antonio Andre <antonioandre1008@gmail.com>
2025-05-13 20:05:50 -0500

    :sparkles: feat: inclui feedback de carregamento de imagem de um post

--- components/posts/ImageCarousel.jsx
+++ components/posts/ImageCarousel.jsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
-import { View, StyleSheet, Dimensions, Image } from 'react-native';
-
-const { width: screenWidth } = Dimensions.get('window');
+import { useState } from 'react';
+import { View, Image, ActivityIndicator } from 'react-native';
 
 function ImageCarousel({ image }) {
   console.log('ImageCarousel - imagem recebida:', image);
+  const [loading, setLoading] = useState(true);
 
   if (typeof image === 'string') {
     return (
       <View className="my-2">
         <View className="h-[250px] w-[calc(100%-32px)] overflow-hidden rounded-lg">
-          <Image source={{ uri: image }} className="h-full w-full" resizeMode="cover" />
+          {loading && (
+            <View className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-white">
+              <ActivityIndicator size="large" color="#0000ff" style={{ alignSelf: 'center' }} />
+            </View>
+          )}
+          <Image
+            source={{ uri: image }}
+            className="h-full w-full"
+            resizeMode="cover"
+            onLoadEnd={() => setLoading(false)}
+          />
         </View>
       </View>
     );
