83d829cabcb2513e67241b77207f08dc9a1af5ed
Antonio Andre <antonioandre1008@gmail.com>
2025-05-13 19:58:07 -0500

    :sparkles: feat: renderiza comentarios

--- app/(app)/_layout.jsx
+++ app/(app)/_layout.jsx
@@ -1,36 +1,47 @@
 import { MaterialIcons } from '@expo/vector-icons';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import * as Haptics from 'expo-haptics';
 import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function AppLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Início',
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="pets" color={color} size={size} />,
-        }}
-        listeners={{
-          tabPress: () => {
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Configurações',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="settings" color={color} size={size} />
-          ),
-        }}
-        listeners={{
-          tabPress: () => {
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
-          },
-        }}
-      />
-    </Tabs>
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <Tabs screenOptions={{ headerShown: false }}>
+          <Tabs.Screen
+            name="home"
+            options={{
+              title: 'Início',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="pets" color={color} size={size} />
+              ),
+            }}
+            listeners={{
+              tabPress: () => {
+                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+              },
+            }}
+          />
+          <Tabs.Screen
+            name="settings"
+            options={{
+              title: 'Configurações',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="settings" color={color} size={size} />
+              ),
+            }}
+            listeners={{
+              tabPress: () => {
+                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+              },
+            }}
+          />
+        </Tabs>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({});
--- components/posts/Post.jsx
+++ components/posts/Post.jsx
@@ -1,8 +1,27 @@
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { BottomSheetModal, BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import React, { useCallback, useRef, useMemo } from 'react';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import ImageCarousel from './ImageCarousel';
 
 export default function Post({ post }) {
+  const bottomSheetModalRef = useRef(null);
+
+  // Apenas uma posição em 50% da altura da tela
+  const snapPoints = useMemo(() => ['50%'], []);
+
+  // Índice inicial deve ser 0 já que só temos um ponto de snap
+  const initialSnapIndex = 0;
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
   return (
     <SafeAreaView>
       <View className="flex flex-row items-center gap-2">
@@ -19,6 +38,30 @@ export default function Post({ post }) {
         </Text>
       </View>
       <ImageCarousel image={post.imagem} />
+      <TouchableOpacity className="size-8" onPress={handlePresentModalPress}>
+        <FontAwesome name="comment-o" size={24} color="black" />
+      </TouchableOpacity>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={initialSnapIndex}
+        snapPoints={snapPoints}
+        enableContentPanningGesture
+        enableHandlePanningGesture
+        handleComponent={() => (
+          <View style={styles.handleContainer}>
+            <View style={styles.handle} />
+          </View>
+        )}
+        onChange={handleSheetChanges}>
+        <BottomSheetScrollView contentContainerStyle={styles.scrollContent}>
+          <Text className="mb-2 text-center text-lg font-bold">Comentários</Text>
+          {post.comentarios_p.map((comentario) => (
+            <Text key={comentario.id_comentario} style={styles.itemText}>
+              {comentario.descricao}
+            </Text>
+          ))}
+        </BottomSheetScrollView>
+      </BottomSheetModal>
       <Text className="mb-1 text-xl font-semibold">{post.titulo}</Text>
       <View className="flex flex-row">
         <Text className="lowercase text-gray-800">
@@ -28,3 +71,40 @@ export default function Post({ post }) {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+  handleContainer: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    alignItems: 'center',
+  },
+  handle: {
+    width: 36,
+    height: 2,
+    borderRadius: 2,
+    backgroundColor: '#000000',
+  },
+  scrollContent: {
+    paddingHorizontal: 16,
+    paddingBottom: 40,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 12,
+    textAlign: 'center',
+  },
+  itemText: {
+    fontSize: 16,
+    padding: 12,
+    marginVertical: 4,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 8,
+  },
+});
